7304e4595ebd79a07b2a7d07d236aa6c79c3609f
Kunal Mittal <84032402+kunalmttl@users.noreply.github.com>
2025-07-10 23:53:13 +0530

    Update rapid-form-filler.user.js

--- rapid-form-filler.user.js
+++ rapid-form-filler.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Form Filler Pro
+// @name         Rapid Form Filler
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  A high-speed, configurable user script to automatically fill Google Forms in milliseconds, designed for time-sensitive applications.
